27fe9988617eaa4c52987a9633c08dd7ae49a9e0
reza <rezaazizsimatupang@gmail.com>
2026-02-12 23:06:26 +0700

    fix: login and register logic + and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import Register from './pages/register'
 import Login from './pages/login'
-import './Index.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Register />
-    </>
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
   )
 }
 
