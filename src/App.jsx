c65828a89d3bf6d4e0ca1faff8fc52b77c9c6373
reza <rezaazizsimatupang@gmail.com>
2026-02-12 19:03:47 +0700

    feat: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Register from './pages/register'
+import Login from './pages/login'
 import './Index.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+    {/* <Login/> */}
     <Register />
     </>
   )
